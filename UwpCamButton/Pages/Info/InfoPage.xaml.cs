7421d6bda37520d2bbbecd3c32665a74a01e248f
Miroslav Stránský <f3n1x.xv@gmail.com>
2021-05-01 11:35:44 +0200

    Revert "Test aktualizace"
    This reverts commit 44c7887c9080b0726bbed772b2f6118b7a28b900.

--- UwpCamButton/Pages/Info/InfoPage.xaml.cs
+++ UwpCamButton/Pages/Info/InfoPage.xaml.cs
@@ -13,7 +13,7 @@ using Windows.UI.Xaml.Controls;
 //using Windows.UI.Xaml.Media;
 //using Windows.UI.Xaml.Navigation;
 
-
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace UwpCamButton
 {
