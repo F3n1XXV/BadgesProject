9958317ce54747114d1d628b72ce96c4b763c647
Miroslav Stránský <f3n1x.xv@gmail.com>
2021-05-01 16:43:27 +0200

    Vycisteni designu codu Pridani navodu instalace

--- UwpCamButton/Pages/Predpripravene_pages(pro_pripadne_rozsireni)/SelectImportPage.xaml.cs
+++ UwpCamButton/Pages/Predpripravene_pages(pro_pripadne_rozsireni)/SelectImportPage.xaml.cs
@@ -3,18 +3,11 @@ using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-//using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace UwpCamButton
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class SelectImportPage : Page
     {
-        public MainPage Mp;
+        public MainPage mainPage;
         public readonly List<(string Tag, Type Page, string Name, int Id)> Pages_list = new List<(string Tag, Type Page, string Name, int Id)>
         {
             ("Camera", typeof(CamPage),"LoadCamera",0),
@@ -34,7 +27,7 @@ namespace UwpCamButton
             Type page = Pages_list.Find(x => x.Tag.ToString() == btn.Tag.ToString()).Page;
 
             if (page != null)
-            { Mp.frame1.Navigate(page, this); }
+                mainPage.frame1.Navigate(page, this);
         }
     }
 }
